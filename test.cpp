8d4789f9f9def3b963daea2f44c3f1dbb5b4d131
AleksandrSedelnikov <aleksandrband2@gmail.com>
2022-10-21 18:10:18 +0700

    'upd:current_time'

--- test.cpp
+++ test.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
 #include <math.h>
+#include <ctime>
 
-int main()
-{
-    cout << "Privet";
+using namespace std;
+
+int main(){
+    time_t now = time(0);
+    char* dt = ctime(&now);
+    system("git add .");
+    system("git commit -m 'upd:current_time'");
+    system("git push origin main");
+    return 0;
 }
