4f18a815b0710a9be69367d6f890ad156bb92024
AleksandrSedelnikov <aleksandrband2@gmail.com>
2022-10-21 19:11:25 +0700

    'upd:testing.'

--- test.cpp
+++ test.cpp
@@ -1,14 +1,23 @@
 #include <iostream>
 #include <math.h>
 #include <ctime>
+#include <string.h>
+#include <stdio.h>
 
 using namespace std;
 
 int main(){
-    time_t now = time(0);
-    char* dt = ctime(&now);
     system("git add .");
-    system("git commit -m upd:*dt");
+    //char d[] = ".";
+    //time_t sec;//
+    //sec = time(NULL);//
+    //char* dt = asctime(localtime(&sec));//
+    //const string time = strcat(dt,d);//
+    //cout<<time<<endl;//
+    //char s[] = "git commit -m 'upd: " + *dt;//
+    //const string cmd = strcat(s,dt);//
+    //cout<<cmd<<endl;// 
+    system("git commit -m 'upd:testing.'");
     system("git push origin main");
     return 0;
 }
