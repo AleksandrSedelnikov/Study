a517966562d3c53f48e1bbb154c773ab072fa097
AleksandrSedelnikov <aleksandrband2@gmail.com>
2022-10-30 21:54:33 +0700

    upd: Sun Oct 30 21:54:33 2022

--- second_course/computer_technology/2laba/laba2.py
+++ second_course/computer_technology/2laba/laba2.py
@@ -36,4 +36,3 @@ for x1 in range(2):
                 y1 = ((not x1) and (not x2) and (not x3) and (not x4)) or ((not x1) and (not x2) and (not x3) and x4) or (x1 and (not x2) and (not x3) and x4) or (x1 and (not x2) and x3 and (not x4)) or (x1 and (not x2) and x3 and x4)
                 y2 = (x1 or x2 or x3 or x4) and (x1 or (not x2) or (not x3) or (not x4)) and ((not x1) or (not x2) or (not x3) or x4)
                 print(x1,"",x2,"",x3,"",x4,"",int(y1),"",int(y2))"""
-
