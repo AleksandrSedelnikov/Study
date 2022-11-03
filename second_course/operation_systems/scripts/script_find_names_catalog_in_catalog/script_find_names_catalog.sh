87ea06f4300f5e5e23c37c15672c3dd69556d401
AleksandrSedelnikov <aleksandrband2@gmail.com>
2022-11-03 13:10:30 +0700

    upd: Thu Nov 03 13:10:30 2022

--- second_course/operation_systems/scripts/script_find_names_catalog_in_catalog/script_find_names_catalog.sh
+++ second_course/operation_systems/scripts/script_find_names_catalog_in_catalog/script_find_names_catalog.sh
@@ -23,32 +23,43 @@ if [ $k = 1 ]
             par=0
             while [ $par == 0 ]
                 do
-                    echo -n "Введите нужный путь: "; read o
-                    for((i=0;i<$k;i++))
-                    do
-                        echo -n "Проверяется путь: ${array[i]} - "
-                        if [ "${array[i]}" == "$o" ]
-                            then
-                                echo -e "\033[32mподошло.\033[0m"
-                                j=${array[i]}
-                                par=1
-                            else
-                                echo -e "\033[31mне подошло.\033[0m"
-                        fi
-                    done
+                    echo -n "Введите нужный путь или код отмены 01: "; read o
+                    if [ "$o" != "01" ]
+                    then
+                        for((i=0;i<$k;i++))
+                        do
+                            echo -n "Проверяется путь: ${array[i]} — "
+                            if [ "${array[i]}" == "$o" ]
+                                then
+                                    echo -e "\033[32mdetected.\033[0m"
+                                    j=${array[i]}
+                                    par=1
+                                else
+                                    echo -e "\033[31mundetected.\033[0m"
+                            fi
+                        done
+                    else
+                        par=1
+                        j=0
+                    fi
             done
-            h=`find $j -type d 2>~/error | wc -l`
-            h1=`expr $h - 1`
-            if [ $h1 != 0 ]
+            if [ "$j" != "0" ]
                 then
-                    l=`echo $j | grep -o "/" | wc -l`
-                    d=`expr $l + 2`
-                    k=`ls -d $j/*/ | cut -f"$d" -d'/'`
-                    echo -e "\033[32mВложенные директории:\033[0m"
-                    echo -e "\033[34m$k\033[0m" 
+                    h=`find $j -type d 2>~/error | wc -l`
+                    h1=`expr $h - 1`
+                    if [ $h1 != 0 ]
+                        then
+                            l=`echo $j | grep -o "/" | wc -l`
+                            d=`expr $l + 2`
+                            k=`ls -d $j/*/ | cut -f"$d" -d'/'`
+                            echo -e "\033[32mВложенные директории:\033[0m"
+                            echo -e "\033[34m$k\033[0m" 
+                        else
+                            echo -e "\033[31mВ каталоге отсутствуют вложенные директории\033[0m"
+                    fi
                 else
-                    echo -e "\033[31mВ каталоге отсутствуют вложенные директории\033[0m"
+                    echo -e "\033[35mИсполнен код выхода из скрипта.\033[0m"
             fi
     else
-        echo -e "\033[41mДанного каталога не существует.\033[0m"
+        echo -e "\033[31mДанного каталога не существует.\033[0m"
 fi
