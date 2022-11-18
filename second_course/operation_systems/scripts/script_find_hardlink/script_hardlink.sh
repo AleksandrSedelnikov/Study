2a1662c6d0db8d730422da6e45aba6f701ed19fd
AleksandrSedelnikov <aleksandrband2@gmail.com>
2022-11-19 01:40:18 +0700

    upd: Sat Nov 19 01:40:18 2022

--- second_course/operation_systems/scripts/script_find_hardlink/script_hardlink.sh
+++ second_course/operation_systems/scripts/script_find_hardlink/script_hardlink.sh
@@ -1,11 +1,11 @@
-a=`ls -la | egrep -v ^d | awk '{print $2 " " $9}' | sed '1d'`
-count=`ls -la | egrep -v ^d | awk '{print $2 " " $9}' | sed '1d' | wc -l`
-for ((i=1;i<=$count;i++))
+count=`ls -la | egrep -v ^d | awk '{print $2 " " $9}' | sed '1d' | wc -l` # количество файлов
+for ((i=1;i<=$count;i++)) # цикл i по количеству файлов
     do
-        line=`ls -la | egrep -v ^d | awk '{print $2 " " $9}' | sed '1d' | sed -n $i"p"`
-        hard=`echo $line | awk '{print $1}'`
-        if [ $hard  -ne 1 ]
+        line=`ls -la | egrep -v ^d | awk '{print $2 " " $9}' | sed '1d' | sed -n $i"p"` # берём строчку
+        hard=`echo $line | awk '{print $1}'` # берём количество жёстких ссылок на файл
+        if [ $hard  -ne 1 ] # проверяем, если количество жёстких ссылок больше 1
             then
-                echo $line | awk '{print $2}' >> result
+                echo $line | awk '{print $2}' >> result # записываем название файла в файл "result"
         fi
 done
+echo "Скрипт завершил свою работу."
