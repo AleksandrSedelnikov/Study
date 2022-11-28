7dd4927cc2406d1ac9adc756343eb5f9060230fd
AleksandrSedelnikov <aleksandrband2@gmail.com>
2022-11-28 22:53:37 +0700

    upd: Mon Nov 28 22:53:37 2022

--- second_course/operation_systems/scripts/script_archive/script_create_archive_vlozhdirect.sh
+++ second_course/operation_systems/scripts/script_archive/script_create_archive_vlozhdirect.sh
@@ -4,15 +4,15 @@ script() {
 	name=""
         if [ -d "$i" ]
         then
-	    name=`echo $i | sed "s/.*\///"`
-        myname=`whoami`
-	    if [ "$name" != "$myname" ] && [ "$name" != "archives" ] # не создавать архив домашней директории и папки с архивами
-	    then
-	    tar -zcvf $name.tar.gz $i # создать архив с содержимым директории
-	    mv $name.tar.gz ~/archives # отправить архив в папку в домашней директории с названием archives
-	    fi
-            script "$i"
-    	fi
+            name=`echo $i | sed "s/.*\///"`
+            myname=`whoami`
+            if [ "$name" != "$myname" ] && [ "$name" != "archives" ] # не создавать архив домашней директории и папки с архивами
+            then
+                tar -zcvf $name.tar.gz $i > /dev/null 2>~/error # создать архив с содержимым директории
+                mv $name.tar.gz ~/archives # отправить архив в папку в домашней директории с названием archives
+            fi
+                script "$i"
+            fi
     done
 }
 script ~
