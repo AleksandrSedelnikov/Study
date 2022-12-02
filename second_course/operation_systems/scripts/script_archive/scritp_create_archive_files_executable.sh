a39a8682bdc2881d4686712db22f797e7e1eaa8a
AleksandrSedelnikov <aleksandrband2@gmail.com>
2022-12-02 23:20:35 +0700

    upd: Fri Dec 02 23:20:35 2022

--- second_course/operation_systems/scripts/script_archive/scritp_create_archive_files_executable.sh
+++ second_course/operation_systems/scripts/script_archive/scritp_create_archive_files_executable.sh
@@ -1,15 +1,19 @@
 script() {
-    for i in "$1"/*
+    for i in "$1"/* # цикл по содержимому директории
     do
-        if [ -d "$i" ]
+        if [ "$1/*" = $i ] # если каталог пустой, то пропускать этот каталог
         then
-            script "$i"
+            continue # скип
+        fi
+        if [ -d "$i" ] # если i это директория
+        then
+            script "$i" # вызов функции с параметром директории
 
-        elif [ -f "$i" ] && [ -x "$i" ]
+        elif [ -f "$i" ] && [ -x "$i" ] # если это файл и он исполняемый
         then
-            echo $i
+            echo $i # вернуть абсолютный путь до файла
         fi
     done
 }
-names=`script ~`
-tar -zcvf testArchive.tar.gz $names
+names=`script ~` # изначальный вызов функции и запись всех возвратов из неё в переменную names
+tar -zcvf testArchive.tar.gz $names # создание сжатого архива с найденными файлами
