5b3b29991c9286bcbd76732b93b6016bf34df557
AleksandrSedelnikov <aleksandrband2@gmail.com>
2023-03-12 17:46:49 +0700

    upd: Sun Mar 12 17:46:49 2023

---
+++ second_course/programming_languages/net_tcp_new/net_client_tcp_new.py
@@ -0,0 +1,43 @@
+import socket
+import datetime
+
+# основная функция клиентской части
+def main():
+    TCP_IP = '127.0.0.1'
+    TCP_PORT = 5022
+    BUFFER_SIZE = 1024
+    print("[Client-Info]: Подключение...")
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        client.connect((TCP_IP, TCP_PORT)) 
+    except ConnectionRefusedError:
+        print('[Client-Info]: Connection reset.')
+        exit(1)
+    print('[Client-Info]: Соединение установлено успешно.')
+    print('Для разрыва соединения с сервером можно воспользоваться следующими способами:\n(1) Сочетание Ctrl + C\n(2) Написать break любым способом в терминал\n\nЧтобы выключить сервер через клиент необходимо ввести: serv off\n\n')
+    while True:
+        try: 
+            x = input("Начните ввод запроса > ")
+            if (x.upper() == 'BREAK'):
+                print('\n[Client-Info]: Выключение клиентской части...')
+                exit(1)
+            if (len(x) == 0):
+                print('Вы не указали сообщение, сообщение ретранслируется обратно...')
+                continue
+            client.send(x.encode('utf-8'))
+            date_send = datetime.datetime.now().strftime("%d.%m.%Y %H:%M:%S")
+            print('= = =\nОтправлено: {}\nВремя отправления: {}\n= = ='.format(x, date_send))
+            response = client.recv(BUFFER_SIZE).decode('utf-8')
+            if not response:
+                print('\n[Client-Info]: Сервер разорвал соединение...')
+                exit(1)
+            date_input = datetime.datetime.now().strftime("%d.%m.%Y %H:%M:%S")
+            print('= = =\nПолучено: {}\nВремя получения: {}\n= = ='.format(response, date_input))
+        except KeyboardInterrupt:
+            print('\n[Client-Info]: Выключение клиентской части...')
+            client.close()
+            break
+    return 0
+
+if __name__ == "__main__":
+    main()    
---
+++ second_course/programming_languages/net_tcp_new/net_serv_tcp_new.py
@@ -0,0 +1,83 @@
+import socket
+import datetime
+global connection_count
+global message_count
+
+def connection(server):
+    global connection_count
+    global message_count
+    connection_count = 1
+    while True:
+        conn, addr = server.accept()
+        print('= = =\n[Server-Info]: Получено соединение [ID {}][IP {} | PORT {}]\n= = ='.format(connection_count,addr[0], addr[1]))
+        start_time = datetime.datetime.now().strftime("%d.%m.%Y %H:%M:%S")
+        message_count = 0
+        result = message_response(conn)
+        if (result == 0):
+            return 0
+        elif (result == 1):
+            print('= = =\n[Server-Info]: Разорвано соединение [ID {}][IP {} | PORT {}]'.format(connection_count,addr[0], addr[1]))
+            date_format = "%d.%m.%Y %H:%M:%S"
+            reset_time = datetime.datetime.now().strftime("%d.%m.%Y %H:%M:%S")
+            buff_time_start = datetime.datetime.strptime(start_time, date_format)
+            buff_time_reset = datetime.datetime.strptime(reset_time, date_format)
+            delta = buff_time_reset - buff_time_start
+            print('Время соединения: {} дней, {} часов, {} минут, {} секунд.\n= = ='.format(delta.days, delta.seconds // 3600, (delta.seconds // 60) % 60, delta.seconds))
+
+        connection_count += 1
+
+
+def message_response(connect):
+    global connection_count
+    global message_count
+    BUFFER_SIZE = 20
+    while True:
+        data = connect.recv(BUFFER_SIZE).decode('utf-8')
+        if not data:
+            return 1
+        if (data.lower() == "serv off"):
+            return 0
+        message_count += 1
+        if (str(data).lower() == "debug"):
+            msg = '\n[Debug-Info]\nНомер соединения: {}\nКоличество сообщений за соединение: {}'.format(connection_count, message_count)
+            connect.send(msg.encode())
+            print('= = =\n[Server-Info]: ВНИМАНИЕ! Была отправлена техническая информация\n= = =')
+            continue
+        date_input = datetime.datetime.now().strftime("%d.%m.%Y %H:%M:%S")
+        print('= = =\nПолучено: {}\nВремя получения: {}\n= = ='.format(data, date_input)) 
+        msg = input('Начните ввод ответа > ')
+        if (len(msg) == 0):
+            print('= = =\n[Server-Error]: Empty data\n= = =')
+            connect.send('К сожалению, сервер не указал ниодного символа в сообщении, повторите запрос...'.encode())
+            continue
+        try:
+            connect.send(msg.encode())
+        except Exception:
+            print('= = =\n[Server-Info]: Соединение с клиентской частью было разорвано...\n= = =')
+            continue
+        date_send = datetime.datetime.now().strftime("%d.%m.%Y %H:%M:%S")
+        print('= = =\nОтправлено: {}\nВремя ответа: {}\n= = ='.format(msg, date_send)) 
+
+
+
+def main():
+    TCP_IP = '127.0.0.1'
+    TCP_PORT = 5022
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server:
+        print("= = =\n[Server-Info]: Открываем INET сокет...")
+        server.bind((TCP_IP, TCP_PORT))
+        server.listen(5)
+        print("[Server-Info]: Сервер настроен на прослушивание...\n= = =")
+        try:
+            if (connection(server) == 0):
+                server.close()
+                print('= = =\n[Server-Info]: Сервер выключен...\n= = =')
+                exit(1)
+        except KeyboardInterrupt:
+            server.close()
+            print("= = =\n[Server-Info]: Сервер выключен...\n= = =")
+    return 0
+
+
+if __name__ == "__main__":
+    main()
