e8766b519d7493b467c4ca52cca1f63f76c9bed7
AleksandrSedelnikov <aleksandrband2@gmail.com>
2023-05-30 21:39:45 +0700

    auto upd: Tue May 30 21:39:44 2023

--- add_git/add_git.cpp
+++ add_git/add_git.cpp
@@ -7,14 +7,27 @@ using namespace std;
 
 int main(){
     system("git add .");
-    time_t sec;
-    sec = time (NULL);
-    string dt = asctime(localtime(&sec));
-    string s = "git commit -m ";
-    string d{'"'};
-    string k = "upd: ";
-    string command = s + d + k + dt + d;
-    system(command.c_str());
+    string var;
+    cin >> var;
+    if (var == "другой") {
+        string commit;
+        cin >> commit;
+        string s = "git commit -m ";
+        string d{'"'};
+        string k = "manual upd: ";
+        string command = s + d + k + commit + d;
+        system(command.c_str());
+    }
+    else {
+        time_t sec;
+        sec = time (NULL);
+        string dt = asctime(localtime(&sec));
+        string s = "git commit -m ";
+        string d{'"'};
+        string k = "auto upd: ";
+        string command = s + d + k + dt + d;
+        system(command.c_str());
+    }
     system("git push origin main");
     return 0;
 }
